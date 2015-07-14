86d5854259803fbcc9e1a621e789f9447325910a
admin <admin@admin-PC>
2015-07-14 08:32:44 +0530

    Weather Commited

---
+++ Weather/src/Weather.java
@@ -0,0 +1,11 @@
+
+public class Weather {
+public static void main(String arsd[])
+{
+	System.out.println("Weather Today");
+	int cel=45;
+	int far=34;
+	System.out.println("Celcius  "+cel);
+	System.out.println("Farenhit "+far);
+}
+}
